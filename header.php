25f7bf8f50072b91d4fe7a8cf4b4b7524ecf7aba
Theo <yungtheo123@googlemail.com>
2020-08-28 00:39:46 +0200

    Styling Nav

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <header class="masthead mb-auto">
     <div class="inner">
-      <h3 class="masthead-brand">CwL</h3>
-      <nav class="nav nav-masthead justify-content-center">
+      <nav class="nav justify-content-center">
         <a class="nav-link active" href="#">Team</a>
         <a class="nav-link" href="#">Kalender</a>
+        <a class="nav-link" href="#"><img src="./img/logo.png" alt="Clan Logo"></a>
 		<a class="nav-link" href="#">Blog</a>
         <a class="nav-link" href="#">Kontakt</a>
       </nav>
