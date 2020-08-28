72777306672135f9d4132385b861134058ddcfec
Theo <yungtheo123@googlemail.com>
2020-08-28 11:58:43 +0200

    navbar styling

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <header class="masthead mb-auto">
     <div class="inner">
-      <nav class="nav justify-content-center">
-        <a class="nav-link active" href="#">Team</a>
-        <a class="nav-link" href="#">Kalender</a>
-        <a class="nav-link" href="#"><img src="./img/logo.png" alt="Clan Logo"></a>
-		<a class="nav-link" href="#">Blog</a>
-        <a class="nav-link" href="#">Kontakt</a>
+      <nav class="nav justify-content-center" id="navbar">
+        <a class="nav-link active nav-elements" href="#">Team</a>
+        <a class="nav-link nav-elements" href="#">Kalender</a>
+        <a class="nav-link nav-elements" href="#"><img src="./img/logo.png" alt="Clan Logo"></a>
+		<a class="nav-link nav-elements" href  ="#">Blog</a>
+        <a class="nav-link nav-elements" href="#">Kontakt</a>
       </nav>
     </div>
   </header>
