0a070b1b086280a4fd398df0f28ee34cb559238f
Theo <yungtheo123@googlemail.com>
2020-08-28 12:43:55 +0200

    Einrückung

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
-<?php include "header.php"?>
+<!doctype html>
+<html lang="en"> 
+  <?php include "header.php"?>
   <body class="text-center">
     <div class="container-fluid d-flex w-100 h-100 p-3 mx-auto flex-column">
-    <?php include "nav.php"?>
+      <?php include "nav.php"?>
 
-  <main role="main" class="inner cover">
-    <h2 class="lead"id="Description">E-Sport & Gaming Clan</h2>
-    <h1 class="cover-heading" id="ClanName">Cowboy with Lamas</h1>
-    <p class="lead">
-      <a href="#" class="btn btn-lg btn-secondary" id="StartButton">Auf gehts Attacke!</a>
-    </p>
-    <?php include "footer.php"?>
-  </main>
-
-
-</div>
-
-</body>
+      <main role="main" class="inner cover">
+        <h2 class="lead"id="Description">E-Sport & Gaming Clan</h2>
+        <h1 class="cover-heading" id="ClanName">Cowboy with Lamas</h1>
+        <p class="lead">
+          <a href="#" class="btn btn-lg btn-secondary" id="StartButton">Auf gehts Attacke!</a>
+        </p>
+      </main>
+      <?php include "footer.php"?>
+    </div>
+  </body>
 </html>
